2873c772edb367aa64f8c568b2f25ab8c43c4142
Pups1488 <pups1488@false.com>
2022-05-19 01:51:32 +0300

    Операторы сравнения
    Каким будет результат этих выражений?

---
+++ comparison/task_first.js
@@ -0,0 +1,7 @@
+5 > 4 // True 
+"ананас" > "яблоко" // True "я"
+"2" > "12" //  True 2 > 1
+undefined == null // True  -> Only undefined == null
+undefined === null // False -> Only undefined == null
+null == "\n0\n" // False -> Only undefined == null 
+null === +"\n0\n" // False -> Only undefined == null 
