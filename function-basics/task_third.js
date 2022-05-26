1fc8173d73bf8373c0a420dcb6d998d8bf71f63b
Pups1488 <pups1488@false.com>
2022-05-26 19:03:57 +0300

    Функция min(a, b)
    Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.
    
    Пример вызовов:
    
    min(2, 5) == 2
    min(3, -1) == -1
    min(1, 1) == 1

---
+++ function-basics/task_third.js
@@ -0,0 +1,8 @@
+function min(first_number,second_number){
+    return first_number > second_number ? second_number : first_number
+}
+
+
+min(2, 5) == 2  // True
+min(3, -1) == -1 // True
+min(1, 1) == 1 // True
