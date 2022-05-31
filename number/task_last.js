1e74d9653c36693c4ddd8470b8a0ad25ee815833
Pups1488 <pups1488@false.com>
2022-05-31 22:26:12 +0300

    Случайное целое число от min до max
    Напишите функцию randomInteger(min, max), которая генерирует случайное
    целое (integer) число от min до max (включительно).
    
    Любое число из интервала min..max должно появляться с одинаковой
    вероятностью.
    
    Пример работы функции:

---
+++ number/task_last.js
@@ -0,0 +1,7 @@
+function randomInteger(min, max) {
+    return Math.floor((Math.random() * max + min))
+}
+
+alert( randomInteger(1, 5) );
+alert( randomInteger(1, 5) );
+alert( randomInteger(1, 5) );
