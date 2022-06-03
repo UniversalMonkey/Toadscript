5d8dad71d5c1d7ca7b2b403613d670de997da9ad
Pups1488 <pups1488@false.com>
2022-06-03 04:46:55 +0300

    Получить средний возраст
    Напишите функцию getAverageAge(users), которая принимает массив объектов
    со свойством age и возвращает средний возраст.
    
    Формула вычисления среднего арифметического значения: (age1 + age2 + ...
    + ageN) / N.

---
+++ array-methods/task_evelen.js
@@ -0,0 +1,12 @@
+function getAverageAge(arr){
+    return  arr.reduce( (a,b) => a + b.age, 0) / arr.length
+}
+
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let arr = [ vasya, petya, masha ];
+
+alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
