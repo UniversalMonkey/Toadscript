f76b58ea315041b9ca4e6430e378b2a5bb182524
Pups1488 <pups1488@false.com>
2022-06-04 18:16:43 +0300

    Хранение отметок "не прочитано"
    Есть массив сообщений.
    
    У вас есть к ним доступ, но управление этим массивом происходит где-то
    ещё. Добавляются новые сообщения и удаляются старые, и вы не знаете в
    какой момент это может произойти.
    
    Имея такую вводную информацию, решите, какую структуру данных вы могли
    бы использовать для ответа на вопрос «было ли сообщение прочитано?».
    Структура должна быть подходящей, чтобы можно было однозначно сказать,
    было ли прочитано это сообщение для каждого объекта сообщения.
    
    P.S. Когда сообщение удаляется из массива messages, оно должно также
    исчезать из структуры данных.
    
    P.P.S. Нам не следует модифицировать сами объекты сообщений, добавлять
    туда свойства. Если сообщения принадлежат какому-то другому коду, то это
    может привести к плохим последствиям.

---
+++ weakmap-weakset/task_first.js
@@ -0,0 +1,9 @@
+
+let messages_read = new WeakSet(messages)
+
+
+let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+  ];
