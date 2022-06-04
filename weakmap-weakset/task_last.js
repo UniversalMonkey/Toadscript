e2879740164982ef899ce02da3fff14231d142de
Pups1488 <pups1488@false.com>
2022-06-04 18:17:29 +0300

    Есть массив сообщений
    Теперь вопрос стоит так: какую структуру данных вы бы предложили
    использовать для хранения информации о том, когда сообщение было
    прочитано?
    
    В предыдущем задании нам нужно было сохранить только факт прочтения «да
    или нет». Теперь же нам нужно сохранить дату, и она должна исчезнуть из
    памяти при удалении «сборщиком мусора» сообщения.
    
    P.S. Даты в JavaScript можно хранить как объекты встроенного класса
    Date, которые мы разберём позднее.

---
+++ weakmap-weakset/task_last.js
@@ -0,0 +1,9 @@
+
+
+let messages_time = WeakMap(messages)
+
+let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+  ];
