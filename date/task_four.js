c1d1d5496d6e682038c1136ad808949ce5e2f74d
Pups1488 <pups1488@false.com>
2022-06-12 22:48:43 +0300

    Создайте функцию getDateAgo(date, days), возвращающую число, которое было days дней назад от даты date.
    К примеру, если сегодня двадцатое число, то getDateAgo(new Date(), 1)
    вернёт девятнадцатое и getDateAgo(new Date(), 2) – восемнадцатое.
    
    Функция должна надёжно работать при значении days=365 и больших
    значениях:

---
+++ date/task_four.js
@@ -0,0 +1,14 @@
+function getDateAgo(date, days) {
+    return new Date( date.getFullYear(), date.getMonth(),
+                    date.getDate() - days )
+}
+
+
+
+let date = new Date(2015, 0, 2);
+
+alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+
+console.log(date)
