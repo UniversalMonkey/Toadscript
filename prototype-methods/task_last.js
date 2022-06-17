7b22ad03b60f96c16e8ec1d4e53cc6c00817794b
Pups1488 <pups1488@false.com>
2022-06-17 07:10:56 +0300

    Разница между вызовами
    Все эти вызовы делают одно и тоже или нет?

---
+++ prototype-methods/task_last.js
@@ -0,0 +1,16 @@
+function Rabbit(name) {
+    this.name = name;
+  }
+  Rabbit.prototype.sayHi = function() {
+    alert(this.name);
+  };
+  
+  let rabbit = new Rabbit("Rabbit");
+
+  console.log(Object.getPrototypeOf(rabbit))
+
+
+rabbit.sayHi(); // Rabbit
+Rabbit.prototype.sayHi(); //undefined
+Object.getPrototypeOf(rabbit).sayHi(); // undefined
+rabbit.__proto__.sayHi(); // undefined
