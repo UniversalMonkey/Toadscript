92bd02bed8e851278b51f190e5bdc2ed2d1543ab
Pups1488 <pups1488@false.com>
2022-06-27 12:52:49 +0300

    Где в DOM-иерархии "document"?
    Объектом какого класса является document?
    
    Какое место он занимает в DOM-иерархии?
    
    Наследует ли он от Node или от Element, или может от HTMLElement?

--- basic-dom-node-properties/task_last.js
+++ basic-dom-node-properties/task_last.js
@@ -1,3 +1,5 @@
+ document // HTMLDocument -> Document -> Node
+ 
  document.__proto__ instanceof Node // true
  document.__proto__ instanceof Element // false
  document.__proto__ instanceof HTMLElement // false
